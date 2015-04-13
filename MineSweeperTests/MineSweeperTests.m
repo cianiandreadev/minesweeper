2169848d123c6305b25d7b7159b0f610b835351e
Andrea Ciani <cianiandrea10@gmail.com>
2015-04-13 22:26:24 +0200

    Initial Commit

---
+++ MineSweeperTests/MineSweeperTests.m
@@ -0,0 +1,40 @@
+//
+//  MineSweeperTests.m
+//  MineSweeperTests
+//
+//  Created by Andrea Ciani on 13/04/15.
+//  Copyright (c) 2015 Ciani Andrea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MineSweeperTests : XCTestCase
+
+@end
+
+@implementation MineSweeperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
